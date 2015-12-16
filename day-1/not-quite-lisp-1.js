590cd332ca598e633a5402613021d30fcb3dffb2
Jordan Gray <j.b.gray@gmail.com>
2015-12-16 13:30:13 +0000

    Alternative day 1, part 1 solution using filter

--- day-1/not-quite-lisp-1.js
+++ day-1/not-quite-lisp-1.js
@@ -1,5 +1,9 @@
 // http://adventofcode.com/day/1
 
+// Find the terminal floor for a set of directions.
 const find_floor = directions =>
   [...directions].reduce((floor, dir) => floor + dir === '(' ? 1 : -1, 0)
-  
+
+// Alternative solution with filter.
+const find_floor_alt = directions =>
+  directions.length - 2 * [...directions].filter(dir => d === ')').length
