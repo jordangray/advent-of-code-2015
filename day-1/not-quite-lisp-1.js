2d10214088170f9aafdbcb3b65e02ab5335ac9bd
Jordan Gray <j.b.gray@gmail.com>
2015-12-17 17:55:39 +0000

    Way simpler solution to day 1, part 1

--- day-1/not-quite-lisp-1.js
+++ day-1/not-quite-lisp-1.js
@@ -1,9 +1,18 @@
 // http://adventofcode.com/day/1
 
 // Find the terminal floor for a set of directions.
-const find_floor = directions =>
-  [...directions].reduce((floor, dir) => floor + dir === '(' ? 1 : -1, 0)
+const find_floor = dirs => dirs.length - 2 * dirs.split(')').length + 2
 
-// Alternative solution with filter.
-const find_floor_alt = directions =>
-  directions.length - 2 * [...directions].filter(dir => d === ')').length
+
+/* EXPLANATION
+
+closing_brackets = dirs.split(')').length - 1               // (1)
+
+opening_brackets = dirs.length - closing_brackets           // (2)
+
+floor = opening_brackets - closing_brackets
+      = dirs.length - closing_brackets - closing_brackets   // Substitute (2)
+      = dirs.length - 2 * closing_brackets                  // Simplify
+      = dirs.length - 2 * (dirs.split(')').length - 1)      // Substitute (1)
+      = dirs.length - 2 * dirs.split(')').length + 2        // Expand
+*/
